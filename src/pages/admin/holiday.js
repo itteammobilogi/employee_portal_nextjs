ee9974815c877b0fe64675758d120cfc94238ab3
akbar-demandesk <adops@demandesk.com>
2025-08-12 11:38:55 +0530

    chnges

---
+++ src/pages/admin/holiday.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function holiday() {
+  return <div>holiday</div>;
+}
+
+export default holiday;
---
+++ src/pages/admin/payroll/payroll.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function payroll() {
+  return <div>payroll</div>;
+}
+
+export default payroll;
