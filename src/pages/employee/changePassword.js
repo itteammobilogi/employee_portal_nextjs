878d28ea5a88dbb75c9e6914f51fa82068b71d6d
akbar-demandesk <adops@demandesk.com>
2025-08-18 12:26:11 +0530

    fix: valid React component names for change-password & id-card

---
+++ src/pages/employee/changePassword.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function changePassword() {
+  return <div>change-password</div>;
+}
+
+export default changePassword;
---
+++ src/pages/employee/idCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function idCard() {
+  return <div>id-card</div>;
+}
+
+export default idCard;
